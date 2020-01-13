dd4adba79a9460464e0b6b24825e3e18ab8a7106
DanielZagorsky <54935303+DanielZagorsky@users.noreply.github.com>
2020-01-13 12:15:28 +0200

    Update Main.js

--- Main.js
+++ Main.js
@@ -7,7 +7,7 @@ app.config(function ($stateProvider, $urlRouterProvider) {
   var WeatherState = {
     name: "weather",
     url: "/weather",
-    templateUrl: 'test.html',
+    templateUrl: 'weather.html',
     controller: "WeatherAppController"
   }
 
